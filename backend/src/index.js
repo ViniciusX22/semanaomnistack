b4565713bea444081522af4f6078275e79712912
Vinícius X <viniciusxavier2212@gmail.com>
2020-02-25 23:31:58 -0300

    Curso finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
+const http = require("http");
+const { setupWebSocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect(
   "mongodb+srv://vinicius:suiciniv@cluster0-pcztv.mongodb.net/test?retryWrites=true&w=majority",
@@ -17,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
